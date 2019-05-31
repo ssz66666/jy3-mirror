288913872df18cd51e449de02c96209a61bb294d
hzw <365912472@qq.com>
2019-05-31 16:56:38 +0800

    55

--- JY3/script/04_program/p_order.lua
+++ JY3/script/04_program/p_order.lua
@@ -421,9 +421,9 @@ t['dig_earthworms'] = function()
     G.wait1('dig_earthworms_over')
     G.remove_program('抓蚯蚓_计时器',1)  
     G.removeUI('v_dig_earthworms') 
-    if G.misc().挖宝次数 ~= math.abs(G.misc().挖宝监听) or G.misc().蚯蚓数量 ~= math.abs(G.misc().蚯蚓监听) then 
-        G.trig_event('强制退出')
-    end
+    -- if G.misc().挖宝次数 ~= math.abs(G.misc().挖宝监听) or G.misc().蚯蚓数量 ~= math.abs(G.misc().蚯蚓监听) then 
+    --     G.trig_event('强制退出')
+    -- end
 end 
 t['拼图_计时器'] = function()
     G.misc().计时器 =  1800.002
