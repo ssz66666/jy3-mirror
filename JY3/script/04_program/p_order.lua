d8801b48ab85d9d52dee845dd5893ecd03abeb33
hzw <365912472@qq.com>
2019-06-11 14:43:05 +0800

    55

--- JY3/script/04_program/p_order.lua
+++ JY3/script/04_program/p_order.lua
@@ -704,7 +704,7 @@ t['通用_战斗飘字']=function(int_位置,int_范围)  --
                 if  ui.getChildByName('hurt').getChildByName(位置[p]).getChildByName('减生命').visible == true then 
                     hurt = tonumber(ui.getChildByName('hurt').getChildByName(位置[p]).getChildByName('减生命').text)
                     G.call('add_role',o_battle[位置[p]] ,15,-hurt)
-                    if  G.call('通用_取得人物特效',o_battle[位置[p]],15) and math.random(100) > 95 and G.call('get_role',o_battle[位置[p]],13) < G.call('get_role',o_battle[位置[p]],1) *0.2 then --npc复生效果
+                    if  G.call('通用_取得人物特效',o_battle[位置[p]],15) and math.random(100) > 95 and G.call('get_role',o_battle[位置[p]],15) < G.call('get_role',o_battle[位置[p]],1) *0.2 then --npc复生效果
                         G.call('set_role',o_battle[位置[p]],15,G.call('get_role',o_battle[位置[p]],1))
                     end                  
                 end 
