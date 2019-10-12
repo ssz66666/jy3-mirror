0c7c63b6b7f251532dfcc88a6ba662118809ec5b
hzw <365912472@qq.com>
2019-10-12 17:28:58 +0800

    5

--- JY3/script/04_program/p_order.lua
+++ JY3/script/04_program/p_order.lua
@@ -3229,7 +3229,7 @@ t['通用_检测装备']=function()
     local o_store = G.QueryName(0x10190001)
     local int_继承个数 = 0
     for i = 201,216 do 
-        if G.call('get_point',i) > 300 then 
+        if G.call('get_point',i) > 400 then 
             result = true
             print('000001')
             break 
@@ -3400,7 +3400,7 @@ t['通用_检测装备']=function()
                 end
                 local int_品质 = o_equip.品质 - 1
                 for p = 3,10 do
-                    if o_equip[属性[p]] > 80 then
+                    if o_equip[属性[p]] > 100 then
                         print('000008')
                         result = true
                         break
