90b43075455aaa92d919184fcc5b1f0db2cbd68e
hzw0827 <365912472@qq.com>
2020-03-07 18:42:25 +0800

    5

--- JY3/script/04_program/p_person.lua
+++ JY3/script/04_program/p_person.lua
@@ -2392,12 +2392,12 @@ t['副本_通天塔']=function(int_模式)
         G.call('notice1','恭喜完成【无尽挑战】') 
     end 
     local int_完成数 = 0
-    for i = 1,#生存成就 do
+    for i = 1,#o_生存成就.进度列表 do
         if o_生存成就.进度列表[i].完成 == 1 then 
             int_完成数 = int_完成数 + 1
         end
     end
-    if 生存成就 == #o_周目成就.进度列表  and o_生存成就.完成 == 0 then
+    if int_完成数 == #o_周目成就.进度列表  and o_生存成就.完成 == 0 then
         --print('int_完成数',int_完成数)
         o_周目成就.完成 = 1
         G.call('notice1','恭喜完成[03]强者生存')
